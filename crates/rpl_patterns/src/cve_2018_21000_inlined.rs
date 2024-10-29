254fc6147cfdd5854bbee1b7540fc2d7be05de39
Frank King <frankking1729@gmail.com>
2024-10-29 20:17:45 +0800

    Fix cve_2018_21000

--- crates/rpl_patterns/src/cve_2018_21000_inlined.rs
+++ crates/rpl_patterns/src/cve_2018_21000_inlined.rs
@@ -1,12 +1,10 @@
-use std::ops::Not;
-
 use rpl_mir::pat::PatternsBuilder;
 use rustc_hir as hir;
 use rustc_hir::def_id::LocalDefId;
 use rustc_hir::intravisit::{self, Visitor};
 use rustc_middle::hir::nested_filter::All;
-use rustc_middle::ty::{self, Ty, TyCtxt};
-use rustc_span::{sym, Span, Symbol};
+use rustc_middle::ty::TyCtxt;
+use rustc_span::Span;
 
 use rpl_mir::{pat, CheckMirCtxt};
 
@@ -50,29 +48,27 @@ impl<'tcx> Visitor<'tcx> for CheckFnCtxt<'tcx> {
             if let mut patterns_reversed_paras = PatternsBuilder::new(&self.tcx.arena.dropless)
                 && let pattern_reversed_para = pattern_reversed_para(&mut patterns_reversed_paras)
                 && let Some(matches) = CheckMirCtxt::new(self.tcx, body, &patterns_reversed_paras.build()).check()
-                && let Some(vec_constructing_stmt) = matches[pattern_reversed_para.vec_constructing_stmt]
-                && let vec_constructing_stmt = vec_constructing_stmt.span_no_inline(body)
+                && let Some(from_raw_parts) = matches[pattern_reversed_para.from_raw_parts]
+                && let span = from_raw_parts.span_no_inline(body)
             {
-                debug!(?vec_constructing_stmt);
-                self.tcx
-                    .dcx()
-                    .emit_err(crate::errors::ReversedParameters { vec_constructing_stmt });
+                debug!(?span);
+                self.tcx.dcx().emit_err(crate::errors::MisorderedParameters { span });
             }
         }
     }
 }
 
-struct PatternReversedPara {
-    vec_constructing_stmt: pat::Location,
+struct PatternMisorderedParam {
+    from_raw_parts: pat::Location,
 }
 
 #[rpl_macros::mir_pattern]
-fn pattern_reversed_para(patterns: &mut pat::PatternsBuilder<'_>) -> PatternReversedPara {
+fn pattern_reversed_para(patterns: &mut pat::PatternsBuilder<'_>) -> PatternMisorderedParam {
     mir! {
         meta!{$T:ty}
 
-        let to_vec: alloc::vec::Vec<$T, alloc::alloc::Global>;
         let from_vec: alloc::vec::Vec<u8, alloc::alloc::Global> = _;
+        let to_vec: alloc::vec::Vec<$T, alloc::alloc::Global>;
         let to_vec_cap: usize;
         let mut from_vec_cap: usize;
         let mut tsize: usize;
@@ -81,23 +77,36 @@ fn pattern_reversed_para(patterns: &mut pat::PatternsBuilder<'_>) -> PatternReve
         let mut from_vec_ptr: core::ptr::non_null::NonNull<u8>;
         let mut to_raw_vec: alloc::raw_vec::RawVec<$T, alloc::alloc::Global>;
         let mut to_raw_vec_inner: alloc::raw_vec::RawVecInner<alloc::alloc::Global>;
-        let mut to_vec_wrapped_len: alloc::raw_vec::Cap = _;
+        let mut to_vec_wrapped_len: alloc::raw_vec::Cap;
         let mut from_vec_unique_ptr: core::ptr::unique::Unique<u8>;
 
         from_vec_ptr = copy from_vec.buf.inner.ptr.pointer;
         from_vec_cap = copy from_vec.buf.inner.cap.0;
         tsize = SizeOf($T);
         to_vec_cap = Div(move from_vec_cap, copy tsize);
-        from_vec_len = copy to_vec.len;
+        from_vec_len = copy from_vec.len;
         to_vec_len = Div(move from_vec_len, copy tsize);
-        // to_vec_wrapped_len = alloc::raw_vec::Cap(copy to_vec_len);
-        from_vec_unique_ptr = core::ptr::unique::Unique<u8> { pointer: copy from_vec_ptr, _marker: core::marker::PhantomData<u8>, };
-        to_raw_vec_inner = alloc::raw_vec::RawVecInner<alloc::alloc::Global> { ptr: move from_vec_unique_ptr, cap: copy to_vec_wrapped_len, alloc: alloc::alloc::Global, };
-        to_raw_vec = alloc::raw_vec::RawVec<$T, alloc::alloc::Global> {inner: move to_raw_vec_inner, _marker: core::marker::PhantomData<$T>, };
-        to_vec = alloc::vec::Vec<$T, alloc::alloc::Global> {buf: move to_raw_vec, len: copy to_vec_cap, };
+        to_vec_wrapped_len = #[ctor] alloc::raw_vec::Cap(copy to_vec_len);
+        from_vec_unique_ptr = core::ptr::unique::Unique<u8> {
+            pointer: copy from_vec_ptr,
+            _marker: core::marker::PhantomData<u8>,
+        };
+        to_raw_vec_inner = alloc::raw_vec::RawVecInner<alloc::alloc::Global> {
+            ptr: move from_vec_unique_ptr,
+            cap: copy to_vec_wrapped_len,
+            alloc: alloc::alloc::Global,
+        };
+        to_raw_vec = alloc::raw_vec::RawVec<$T, alloc::alloc::Global> {
+            inner: move to_raw_vec_inner,
+            _marker: core::marker::PhantomData<$T>,
+        };
+        to_vec = alloc::vec::Vec<$T, alloc::alloc::Global> {
+            buf: move to_raw_vec,
+            len: copy to_vec_cap,
+        };
     }
 
-    PatternReversedPara {
-        vec_constructing_stmt: to_vec_stmt,
+    PatternMisorderedParam {
+        from_raw_parts: to_vec_stmt,
     }
 }
