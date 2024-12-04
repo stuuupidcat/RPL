c93457712c3fbba293db738a2961719a154b5a51
stuuupidcat <stuuupidcat@163.com>
2024-12-04 14:27:49 +0800

    Implementing the MIR pattern of CVE_2018-21000
    Currently, each statement can be matched, but the graph can not be matched. The refactoring of the graph-match-algorithm is still in progress.

--- tests/ui/cve_2018_21000/cve_2018_21000.rs
+++ tests/ui/cve_2018_21000/cve_2018_21000.rs
@@ -1,6 +1,3 @@
-// TOOD: remove this line
-//@ compile-flags: -Z mir-opt-level=3
-
 use std::mem::{forget, size_of};
 use std::vec::Vec;
 
@@ -14,13 +11,14 @@ pub unsafe fn guarded_transmute_vec_permissive<T>(mut bytes: Vec<u8>) -> Vec<T> 
     //~^ ERROR: misordered parameters `len` and `cap` in `Vec::from_raw_parts`
 }
 
-/* pub unsafe fn guarded_transmute_to_bytes_vec<T>(mut from: Vec<T>) -> Vec<u8> {
+pub unsafe fn guarded_transmute_to_bytes_vec<T>(mut from: Vec<T>) -> Vec<u8> {
     let capacity = from.capacity() * size_of::<T>();
     let len = from.len() * size_of::<T>();
     let ptr = from.as_mut_ptr();
     forget(from);
     Vec::from_raw_parts(ptr as *mut u8, capacity, len)
-} */
+    //~^ ERROR: misordered parameters `len` and `cap` in `Vec::from_raw_parts`
+}
 
 fn main() {
     let vec = vec![1, 2, 3, 4, 5];
