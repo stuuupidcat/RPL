stuuupidcat/RPL

commit a261419d8fdba9a6155579f86865a912193204f9

Filter out crates that are not directly depended during resolving item path
This helps to avoid duplicate results.

crates/rpl_mir/src/resolve.rs
@@ -326,6 +326,10 @@ pub fn find_crates(tcx: TyCtxt<'_>, name: Symbol) -> Vec<Res> {
         .iter()
         .copied()
         .filter(move |&num| tcx.crate_name(num) == name)
+        .filter(move |&num| {
+            // Find crates that are directly depended by local crate.
+            tcx.extern_crate(num).map(|krate| krate.is_direct()).unwrap_or(false)
+        })
         .map(CrateNum::as_def_id)
         .map(|id| Res::Def(tcx.def_kind(id), id))
         .collect()

commit 2a3def0fd2682568f3034878e07e755cc5a41bdd

Fix `find_crates`.
Some crates are not directly depended by local crate, but it's supposed to be accessible in pattern, such as `alloc`.

This error is introduced in a261419d8fdba9a6155579f86865a912193204f9

crates/rpl_mir/src/resolve.rs
@@ -327,8 +327,11 @@ pub fn find_crates(tcx: TyCtxt<'_>, name: Symbol) -> Vec<Res> {
         .copied()
         .filter(move |&num| tcx.crate_name(num) == name)
         .filter(move |&num| {
-            // Find crates that are directly depended by local crate.
-            tcx.extern_crate(num).map(|krate| krate.is_direct()).unwrap_or(false)
+            // Find crates that are 
+            // either has been included as a part of prelude
+            // or directly depended by local crate
+            matches!(name.as_str(), "std" | "core" | "alloc")
+                || tcx.extern_crate(num).map(|krate| krate.is_direct()).unwrap_or(false)
         })
         .map(CrateNum::as_def_id)
         .map(|id| Res::Def(tcx.def_kind(id), id))