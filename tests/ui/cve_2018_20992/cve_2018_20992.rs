ac47fea081d70d81b5128d16f5d6992efcb395ca
stuuupidcat <stuuupidcat@163.com>
2024-12-29 22:27:16 +0800

    Use existed pattern to match CVE-2021-45688
    Update docs

--- tests/ui/cve_2018_20992/cve_2018_20992.rs
+++ tests/ui/cve_2018_20992/cve_2018_20992.rs
@@ -5,7 +5,7 @@ pub fn ensure_buffer_len(mut buffer: Vec<i32>, new_len: usize) -> Vec<i32> {
         }
         unsafe {
             buffer.set_len(new_len);
-            //~^ ERROR: Use `Vec::set_len` to extend the length of a `Vec`, including uninitialized elements
+            //~^ ERROR: Use `Vec::set_len` to extend the length of a `Vec`, potentially including uninitialized elements
         }
     } else {
         buffer.truncate(new_len);
---
+++ tests/ui/cve_2021_45688/cve_2021_45688.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::slice;
+
+pub fn read_spv<R: io::Read + io::Seek>(x: &mut R) -> io::Result<Vec<u32>> {
+    let size = x.seek(io::SeekFrom::End(0))?;
+    if size % 4 != 0 {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "input length not divisible by 4",
+        ));
+    }
+    if size > usize::max_value() as u64 {
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "input too long"));
+    }
+    let words = (size / 4) as usize;
+    let mut result = Vec::<u32>::with_capacity(words);
+    x.seek(io::SeekFrom::Start(0))?;
+    unsafe {
+        x.read_exact(slice::from_raw_parts_mut(
+            result.as_mut_ptr() as *mut u8,
+            words * 4,
+        ))?;
+        result.set_len(words);
+        //~^ ERROR: Use `Vec::set_len` to extend the length of a `Vec`, potentially including uninitialized elements
+    }
+    const MAGIC_NUMBER: u32 = 0x0723_0203;
+    if !result.is_empty() && result[0] == MAGIC_NUMBER.swap_bytes() {
+        for word in &mut result {
+            *word = word.swap_bytes();
+        }
+    }
+    if result.is_empty() || result[0] != MAGIC_NUMBER {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "input missing SPIR-V magic number",
+        ));
+    }
+    Ok(result)
+}
