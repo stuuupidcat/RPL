d070b1e12df71a00506803949139594163bfd9dd
stuuupidcat <stuuupidcat@163.com>
2025-01-17 17:05:50 +0800

    Update rpl meta pass errors

--- crates/rpl_parser/src/lib.rs
+++ crates/rpl_parser/src/lib.rs
@@ -3,4 +3,7 @@ pub mod parser;
 pub mod position;
 pub mod span;
 
+pub use error::ParseError;
 pub use parser::{generics, pairs, rules, Grammar, Rule};
+pub use position::PositionWrapper;
+pub use span::SpanWrapper;
