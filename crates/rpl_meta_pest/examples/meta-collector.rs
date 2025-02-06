2d0a39c8325bf3c7b63d7e23d9eb077d315539fb
stuuupidcat <stuuupidcat@163.com>
2025-02-06 21:29:38 +0800

    Update
    part of the pest parser

--- crates/rpl_meta_pest/examples/meta-collector.rs
+++ crates/rpl_meta_pest/examples/meta-collector.rs
@@ -10,15 +10,30 @@
 #![feature(iterator_try_collect)]
 #![feature(cell_update)]
 
+extern crate rustc_span;
+
+use std::io::stderr;
+
+use rpl_meta_pest::cli::collect_file_cli;
 use rpl_meta_pest::context::RPLMetaContext;
 use rpl_meta_pest::meta::RPLMeta;
-use rpl_utils::cli::cli;
-use rpl_utils::unwrap;
 
 fn main() {
-    cli(|input, path| {
+    rustc_span::create_session_if_not_set_then(rustc_span::edition::LATEST_STABLE_EDITION, |_| {
         let mut mctx = RPLMetaContext::default();
-        let x = unwrap!(RPLMeta::collect_from_path(path.into(), input.into(), &mut mctx));
-        "Succeed".to_string()
+        let vec = collect_file_cli();
+        for (buf, path) in vec {
+            let meta = RPLMeta::parse_and_collect(path, buf, &mut mctx);
+            match meta {
+                Ok(meta) => {
+                    meta.show_error(&mut stderr());
+                    mctx.add_meta(meta.idx, meta);
+                },
+                Err(meta) => {
+                    // display error
+                    eprintln!("{}", meta);
+                },
+            }
+        }
     });
 }
