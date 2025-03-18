51095169c7ecf67a02b5d5488be3fd5ffb555ce1
TheVeryDarkness <3266343194@qq.com>
2025-03-18 10:40:16 +0800

    Update document

---
+++ tests/ui/cve_2021_25905/minimal-struct.rs
@@ -0,0 +1,18 @@
+//@ revisions: inline regular
+//@[inline] compile-flags: -Z inline-mir=true
+//@[regular] compile-flags: -Z inline-mir=false
+
+#[derive(Default)]
+pub struct Wrapper {
+    buf: Vec<u8>,
+}
+
+// #[rpl::dump_mir(dump_cfg, dump_ddg)]
+fn main() {
+    let mut wrapped: Wrapper = Wrapper::default();
+    let b = wrapped.buf.len();
+    let buf = unsafe {
+        std::slice::from_raw_parts_mut(wrapped.buf.as_mut_ptr().offset(b as isize), wrapped.buf.capacity() - b)
+        //~^ ERROR: it violates the precondition of `std::slice::from_raw_parts_mut` to create a slice from uninitialized data
+    };
+}
