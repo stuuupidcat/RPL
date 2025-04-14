975f104ab9f6f5658fc7a26c9a5f620c988f39de
stuuupidcat <stuuupidcat@163.com>
2025-04-14 16:09:28 +0800

    Update names
    - fix typo
    - remove useless hyphens

--- tests/ui/std/mem/zeroed/zeroed.rs
+++ tests/ui/std/mem/zeroed/zeroed.rs
@@ -1,13 +1,13 @@
 //@ignore-on-host
 
 #[rpl::dump_mir()]
-fn zeroed_inited_reference<T>() -> &'static T {
+fn zeroed_initialized_reference<T>() -> &'static T {
     let x: &'static T = unsafe { std::mem::zeroed() };
     x
 }
 
 #[rpl::dump_mir()]
-fn zeroed_inited_reference_mut<T>() -> &'static mut T {
+fn zeroed_initialized_reference_mut<T>() -> &'static mut T {
     let x: &'static mut T = unsafe { std::mem::zeroed() };
     x
 }
