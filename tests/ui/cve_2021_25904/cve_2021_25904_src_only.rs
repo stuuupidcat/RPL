adfab191b1426ab2713248cd70f81aa386066d97
TheVeryDarkness <3266343194@qq.com>
2025-04-19 17:13:08 +0800

    Merge similar ui-test groups

--- tests/ui/cve_2021_25904/cve_2021_25904_src_only.rs
+++ tests/ui/cve_2021_25904/cve_2021_25904_src_only.rs
@@ -1,7 +1,11 @@
+//@ revisions: inline regular
+//@[inline] compile-flags: -Z inline-mir=true
+//@[regular] compile-flags: -Z inline-mir=false
+
 use std::slice;
 
 // #[rpl::dump_mir(dump_cfg, dump_ddg)]
-pub fn copy_from_raw_parts<I, IU>(mut src: I, mut src_linesize: IU)
+pub fn copy_from_raw_parts_iterator_next<I, IU>(mut src: I, mut src_linesize: IU)
 //~^ERROR: it is unsound to trust pointers from passed-in iterators in a public safe function
 where
     I: Iterator<Item = *const u8>,
@@ -10,6 +14,32 @@ where
     let rr = src.next().unwrap();
     let s_linesize = src_linesize.next().unwrap();
     let ss = unsafe { slice::from_raw_parts(rr, s_linesize) };
+}
+
+// #[rpl::dump_mir(dump_cfg, dump_ddg)]
+pub fn copy_from_raw_parts_for_iterator<I, IU>(mut src: I, mut src_linesize: IU)
+where
+    I: Iterator<Item = *const u8>,
+    IU: Iterator<Item = usize>,
+{
+    for rr in src {
+        let s_linesize = src_linesize.next().unwrap();
+        let ss = unsafe { slice::from_raw_parts(rr, s_linesize) };
+        //FIXME: detect this case
+    }
+}
+
+// #[rpl::dump_mir(dump_cfg, dump_ddg)]
+pub fn copy_from_raw_parts_for_iterator_zip<I, IU>(mut src: I, mut src_linesize: IU)
+where
+    I: Iterator<Item = *const u8>,
+    IU: Iterator<Item = usize>,
+{
+    for (rr, s_linesize) in src.zip(src_linesize) {
+        let ss = unsafe { slice::from_raw_parts(rr, s_linesize) };
+        // ...
+        //FIXME: detect this case
+    }
 }
 
 fn main() {
